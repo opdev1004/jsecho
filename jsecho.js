b53eb46ca00e2730df4becefa4d1b48f218e52f2
Victor Chanil Park <opdev1004@gmail.com>
2020-04-25 12:38:38 +1200

    fixed some wrong naming.

--- jsecho.js
+++ jsecho.js
@@ -10,12 +10,12 @@ function echo(x)
   var echo_index = document.getElementsByTagName("script").length - 1;
   var echo_content = x;
   // Check for null and undefined and making sure boolean is printed.
-  if(typeof comment === 'undefined') echo_content = "undefined";
-  else if(comment === null) echo_content = "null";
-  else if(typeof echo_content === 'boolean')
+  if(typeof echo_content === "undefined") echo_content = "undefined";
+  else if(echo_content === null) echo_content = "null";
+  else if(typeof echo_content === "boolean")
   {
-    if(echo_content) echo_content = "TRUE";
-    else echo_content = "FALSE";
+    if(echo_content) echo_content = "true";
+    else echo_content = "false";
   }
   document.getElementsByTagName("script")[echo_index].outerHTML = echo_content + document.getElementsByTagName("script")[echo_index].outerHTML ;
 }
